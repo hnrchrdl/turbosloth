9900b8e0a49c37ae5ad25590232cee0e2bdd40d9
hnrchrdl <hinrich@posteo.de>
2014-08-30 23:06:46 +0200

    better socket use. mpd status indicators added

--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,82 @@ var router = express.Router();
 
 var komponist = require('komponist');
 var komponistClient;
-//var mpdClient;
 
-//var io = require('../lib/sockets');
 var socketio = require('socket.io');
-var io, socket_emit, sendMpdAorta;
-//var io = socketio.listen(app);
-//module.exports.listen = function(app){
+var io, socketEmit, sendMpdAorta;
+
+
+// *************************
+// *** set up the socket ***
+
 var listen = function(app) {
+
 	io = socketio.listen(app);
+	
 	io.sockets.on('connection', function (socket) {
 		console.log('A socket connected');
 		
 		socket.on('requestMpdAorta', function() {
+			// sends the App Aorta
 			sendMpdAorta();
 		});
 
+		
+		socket.on('mpdCommand', function(msg) {
+			// listens for Mpd Commands
+
+			console.log('cmd: ' + JSON.parse(msg).cmd);
+			console.log('args: ' + JSON.parse(msg).args);
+
+			// cmd is the mpd command as a String 
+			cmd = JSON.parse(msg).cmd;
+			// args are the optional commands as Array
+			args = JSON.parse(msg).args;
+			
+			//console.log(args.length);
+
+			if (args.length === 0) {
+				console.log('here');
+				// execute mpd command WITHOUT args
+				komponistClient[cmd](function(err,msg) {
+					console.log(err);
+					console.log(msg);
+				});
+			}
+			else if (args.length === 1 ) {
+				// execute mpd command WITH ONE arg
+				komponistClient[cmd](args[0],function(err,msg) {
+					console.log(err);
+					console.log(msg);
+				});
+			}
+			else if (args.length === 2 ) {
+				// execute mpd command WITH TWO arg
+				komponistClient[cmd](args[0],args[1],function(err,msg) {
+					console.log(err);
+					console.log(msg);
+				});
+			}
+		});
+
+
 		sendMpdAorta = function () {
+			// sends the App Aorta
+			// this is done when side is lodaed
+			// or something on Mpd has happend 
+
 			console.log('mpd aorta has been requested');
 			komponistClient.status(function(err,msg) {
+				
+				// extract status
 				var status = JSON.stringify(msg);
 				komponistClient.currentsong(function(err,msg) {
+					
+					// extract current song
 					currentsong = JSON.stringify(msg);
 					console.log(status);
+
+					// emit data to client 
 					socket.emit('sendMpdAorta', {
 						status:status,
 						currentsong:currentsong 
@@ -35,13 +88,20 @@ var listen = function(app) {
 			});
 		}
 
-		socket_emit = function(name,data) {
-			socket.emit(name,data);
-		};
+		//socketEmit = function(name, data) {
+			// sends data emits to the client
+
+		//	socket.emit(name,data);
+		//};
 	});
-	return io;
 	
+	// make io availible to other modules
+	return io;	
 }
+
+
+//  ******************
+//  *** the routes ***
 
 router.get('/', function(req, res) {
 
@@ -63,26 +123,16 @@ router.get('/', function(req, res) {
 		});
 		komponistClient.on('changed', function(system) {
 			console.log('Subsystem changed: ' + system);
-			if (system == 'player') {
+			if (system == 'player' || system == 'options') {
 				sendMpdAorta();
-				/*komponistClient.currentsong(function(err,data) {
-					if (data && data != {}) {
-						//console.log(data);
-						socket_emit('changedPlayer', JSON.stringify(data));
-					}
-					else {
-						res.write("OK");
-					}
-				});*/
 			}
 		});
 		
-		//render page
+		//render skeleton
 		res.render('app', { 
 			title: 'leukosia node', 
 			mpdhost: req.session.mpdhost,
 			mpdport: req.session.mpdport
-			//mpdClient: mpd_client,
 		});
 	}
 	else {
