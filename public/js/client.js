bd7c1863e650a8fb2b930005d8bf644b84a9735b
hnrchrdl <hinrich@posteo.de>
2014-10-20 17:31:01 +0200

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -35,158 +35,30 @@ function playerHasChanged(init) {
 
 function queueRequest() {
   var q = new Queue(function(err, queue) {
-    if (queue) { queue.render(); }
-    else { console.log(err); }
+    if (err) { console.log(err); }
+    else if (queue) { queue.render(); }
   });
 }
 
-
-
-function renderPlaylists() {
-  // manage playlist click
-  $.ajax({
-    url: '/manageplaylist'
-  }).done(function(html){
-    // inject html
-    $('main').html(html);
-    fixScrollHeight();
-    $('nav').find('.loading.playlists').hide();
-    
-    
+function playlistsRequest() {
+  var p = new Playlists(function(err, playlists) {
+    if (err) {console.log(err); }
+    else if (playlists) { playlists.render(); }
   });
 }
-
-
-function renderBrowse(folder) {
-  $('nav').find('.button').removeClass('active');
-  $('nav').find('.button.browse').addClass('active');
-  $('nav').find('.loading.browse').show();
-  $.ajax({
-    url: 'browse/' + encodeURIComponent(folder)
-  }).done(function(html){
-
-    $('main').html(html);
-    fixScrollHeight();
-    $('nav').find('.loading.browse').hide();
-        
-    $('#browse').find('.dir').find('a').off('click');
-    $('#browse').find('.dir').find('a').on('click', function(e){
-      e.preventDefault();
-      renderBrowse($(this).attr('data-dir'));
-    });
-    $('#browse').find('.dir-info').find('a').off('click');
-    $('#browse').find('.dir-info').find('a').on('click', function(e){
-      e.preventDefault();
-      renderBrowse("--" + $(this).attr('data-dir'));
-    });
-
-    // register Buttons
-    $('#browse').on('click', '.append.button', function(){
-      var dir = $(this).parents('.dir').attr('data-directory');
-      socket.emit('mpd', 'add', [dir], function(err,msg){
-        if (err) {
-          console.log(err);
-        }
-        else {
-          $('nav').find('.button').removeClass('active');
-          $('nav').find('.button.queue').addClass('active');
-          renderQueue();
-        }
-      });
-    });
-
-    $('#browse').find('.load.button').off('click');
-    $('#browse').find('.load.button').on('click', function(){
-      var dir = $(this).parents('.dir').attr('data-directory');
-      socket.emit('mpd', 'clear', [], function(err,msg) {
-        socket.emit('mpd', 'add', [dir], function(err,msg){
-          if (err) {
-            console.log(err);
-          }
-          else {
-            $('nav').find('.button').removeClass('active');
-            $('nav').find('.button.queue').addClass('active');
-            renderQueue();
-          }
-        });
-      });
-    });
+function browseRequest(folder) {
+  var b = new Browse(folder, function(err, browse) {
+    if (err) { console.log(err); }
+    else if (browse) { browse.render(folder); }
   });
 }
-
-function renderSearch(searchString, searchType) {
-  $('nav').find('.button').removeClass('active');
-  $('nav').find('.button.search').addClass('active');
-  $('nav').find('.loading.search').show();
-  
+function searchRequest(searchString, searchType) {
   if (searchString === "" || searchString === " " ) {
     searchString = "#";
   }
-  $.ajax({
-    url: 'search/' + encodeURIComponent(searchString) + "/" + searchType
-  }).done(function(html){
-
-    $('main').html(html);
-    fixScrollHeight();
-    $('nav').find('.loading.search').hide();  
-
-    // register Buttons
-    $('#search').on('change', 'select', function() {
-      var searchString = $('#search').find('input.search-input').val();
-      var searchType = $('#search').find('select.search-select').val();
-      renderSearch(searchString, searchType);
-    });
-    
-    $('#search').on('keyup', 'input.search-input', function(e) {
-      if ( e.which === 13 ) {
-        var searchString = $('#search').find('input.search-input').val();
-        var searchType = $('#search').find('select.search-select').val();
-        renderSearch(searchString, searchType);
-      }
-    });
-
-    // click append
-    $('#search').on('click', '.append.button', function(){
-      var dir = $(this).parents('.dir').attr('data-file');
-      console.log(dir);
-      socket.emit('mpd', 'add', [dir], function(err,msg){
-        if (err) {
-          console.log(err);
-        }
-      });
-    });
-
-    // click advanced
-    $('#search').on('click','.advanced', function() {
-      if ($(this).find('i').hasClass('fa-angle-down')) {
-        $(this).find('i').removeClass('fa-angle-down');
-        $(this).find('i').addClass('fa-angle-up');
-        $(this).parents('.dir').height($(this).parents('.dir').height()*2);
-      } else {
-        $(this).find('i').removeClass('fa-angle-up');
-        $(this).find('i').addClass('fa-angle-down');
-        $(this).parents('.dir').height($(this).parents('.dir').height()/2);
-      }
-    });
-
-    // click search artist
-    $('#search').on('click', '.search', function() {
-      var artist = $(this).parents('.dir').find('.attr.artist').text();
-      renderSearch(artist, 'Artist');
-    });
-
-    // click lookup
-    $('#search').on('click', '.lookup', function() {
-      try {
-        var directory = ($(this).parents('.dir').attr('data-file').split('/'));
-        directory.pop();
-        directory = directory.join('/');
-        console.log(directory);
-        renderBrowse(directory);
-      } catch (e) {}
-    });
-
-
-  });
+  var s = new Search(searchString, searchType, function(err, search) {
+    if (err) { console.log(err); }
+    else if (search) { search.render(); }
+  }
 }
 
