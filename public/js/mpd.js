a0e7158be7ed4f37b8f4ea6da64554660e72d7d3
hnrchrdl <hinrich@posteo.de>
2014-10-20 13:45:15 +0200

    Update mpd.js

--- public/js/mpd.js
+++ public/js/mpd.js
@@ -5,11 +5,17 @@ var Aorta = function(callback) {
   // Object with the current song and status
   var aorta = this;
   socket.emit('mpd', 'currentsong', [], function(err, data) {
-    if (err) { console.log(err); }
+    if (err) { 
+      console.log(err);
+      aorta.song = undefined;
+    }
     else{ aorta.song = data; }
     socket.emit('mpd', 'status', [], function(err, data) {
-      if (err) { console.log(err); }
-      else {aorta.status = data; }
+      if (err) { 
+        console.log(err);
+        aorta.status = undefined;
+      }
+      else { aorta.status = data; }
       
       registerMpdInterface(aorta.status);
       
@@ -17,6 +23,69 @@ var Aorta = function(callback) {
     });
   });
 };
+// Prototypes
+Aorta.prototype.renderCurrentSong = function() {
+  var song = this.song;
+  
+  // display the currently playing song
+  var currentsong = $('#left').find('.currentsong');
+  if (status.state === 'stop') {
+    currentsong.text("");
+  }
+  else {
+    currentsong.html(song.Artist+ '<br>' + 
+        song.Title + '<br>' + '<span class="muted">' + 
+        song.Album + '</span>');
+  }
+  
+  // fetch album cover
+  //fetch_album_cover(song.Artist, song.Album, function(url) {
+  //  console.log(url);
+  //});
+
+  this.highlightSongInQueue();
+  this.renderProgressBar();
+}
+Aorta.prototype.highlightSongInQueue = function() {
+  var song = this.song;
+  
+  $('#queue').find('.song').removeClass('active');
+  $('#queue').find('.song').find('.attr.songpos').removeClass('active');
+  $('#queue').find('.song.' + song.Id).addClass('active');
+  $('#queue').find('.song.' + song.Id).find('.attr.songpos').addClass('active');
+}
+Aorta.prototype.renderProgressBar = function() {
+  var status = this.status;
+  
+  var progressBar = $('#seek-bar');
+  // start
+  var start = function startProgressbar (songTime,elapsed) {
+    var initial_width = elapsed / songTime * 100; 
+    var duration = songTime - elapsed;
+    progressBar
+      .stop()
+      .css('width',initial_width + '%')
+      .animate({'width': '100%'},duration * 1000, 'linear');
+  };
+  // stop
+  var stop = function stopProgressBar () {
+    progressBar.stop();
+  };
+
+  switch (status.state) {
+    case 'play':
+      var songTime = parseFloat(status.time.split(":")[1]);
+      var elapsed = parseFloat(status.elapsed);
+      start(songTime,elapsed);
+      break;
+    case 'pause':
+      stop();
+      break;
+    case 'stop':
+      stop();
+      progressBar.css('width',0);
+  }
+}
 
 //// Queue
 // Constructor
