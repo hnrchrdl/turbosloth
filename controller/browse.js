dbd531a8b4eedb8d81343d64f3e15b5dba8cd538
hnrchrdl <hinrich@posteo.de>
2015-01-20 12:38:50 +0100

    Update browse.js

--- controller/browse.js
+++ controller/browse.js
@@ -1,6 +1,35 @@
 var browse = require('../models/browse');
 
 
+module.exports = {
+  getBrowseData: getBrowseData
+}
+
+//////////////////////////////////////////////
+
+
+/* browse mpd server */
+
+function browse(req,res) {
+  var path = (typeof req.params.path != 'undefined') ? 
+    req.params.path :
+    ""; // if url contains no path variable
+  
+  var options = {
+    sessionID: req.sessionID,
+    host: req.session.mpdhost,
+    port: req.session.mpdport,
+    password: req.session.mpdpassword,
+    cmd: 'lsinfo',
+    args: [path]
+  }
+  browse.fetchFromMpd(options, function(err, data) {
+    return res.json({err: err, contents: data});
+  });
+}
+
+
+///////// old stuff /////////////////////////
 /**
 *** render the browse page 
 *** the path is passed via query
