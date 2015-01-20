4e92fcfea0b5c6cf6c0bc5481c9ceb2528a25201
hnrchrdl <hinrich@posteo.de>
2015-01-20 12:14:57 +0100

    Update playlists.js

--- controller/playlists.js
+++ controller/playlists.js
@@ -1,9 +1,63 @@
 var playlists = require('../models/playlists');
 
+module.exports = {
+  getList: getList,
+  getByName: getByName
+}
+
+
+
+////////////////////////////////////////////////
+
+
+/* respond with a list of all playlists as json */
+
+function getAll(req,res) {
+  
+  var options = {
+    sessionID: req.sessionID,
+    host: req.session.mpdhost,
+    port: req.session.mpdport,
+    password: req.session.mpdpassword,
+    cmd: 'listplaylists',
+    args: []
+  }
+  
+  playlists.fetchAllFromMpd(options, function(err, data){
+    return res.json({err: err, playlists:data});
+  });
+}
+
+
+/* respond with details of a specific playlists as json */
+
+function getByName(req,res) {
+  var name = req.params.name;
+  var options = {
+    sessionID: req.sessionID,
+    host: req.session.mpdhost,
+    port: req.session.mpdport,
+    password: req.session.mpdpassword,
+    cmd: 'listplaylistinfo',
+    args: [name]
+  }
+  playlists.fetchDetailsFromMpd(options, function(err, data){
+    return res.json({err: err, playlists: data});
+  });
+}
+
+
+
+
+
+
+///// old stuff /////////////////////////////
+
+
 /**
 *** get the Playlists from the model and render it
 **/
-module.exports.renderPlaylists = function(req, res) {
+function renderPlaylists(req, res) {
   var options = {
     sessionID: req.sessionID,
     host: req.session.mpdhost,
@@ -16,12 +70,13 @@ module.exports.renderPlaylists = function(req, res) {
     if (err) return res.render('playlists', {playlists:{}});
     return res.render('playlists', {playlists:data});
   });
-};
+}
+
 
 /**
 *** get Playlist Details from the model and render it
 **/
-module.exports.renderPlaylistDetails = function(req, res) {
+function renderPlaylistDetails(req, res) {
   var playlist = decodeURIComponent(req.params.playlist);
   var options = {
     sessionID: req.sessionID,
@@ -34,4 +89,4 @@ module.exports.renderPlaylistDetails = function(req, res) {
   playlists.fetchDetailsFromMpd(options, function(err, data) {
     res.render('playlistdetails', {playlist: playlist, contents: data});
   });
-};
+}
