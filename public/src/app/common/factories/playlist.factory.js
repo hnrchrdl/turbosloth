a1539722df02b5540fb9f909ea108ddbda403d95
hnrchrdl <hinrich@posteo.de>
2015-01-20 17:17:32 +0100

    Update playlist.factory.js

--- public/src/app/common/factories/playlist.factory.js
+++ public/src/app/common/factories/playlist.factory.js
@@ -3,8 +3,35 @@
   angular.module('app')
     .factory('playlistsFactory', PlaylistsFactory);
     
-  function PlaylistFactory() {
+  function PlaylistsFactory($http, $q) {
+    /* Queue Factory */
     
+    return {
+      getPlaylists: getPlaylists
+    };
+
+    ///////////////////////////////
+
+    function getPLaylists(playlistname) {
+      console.log('Playlists Request');
+      
+      var playlistname = (typeof(playlist) != 'undefined') ?
+        playlistname :
+        null;
+      
+      var deferred = $q.defer();
+      
+      if (playlistname) {
+        var http = $http.get('/api/playlist/' + playlistname);
+      } else {
+        var http = $http.get('/api/playlists')
+      }
+      http
+        .success(function(data) { deferred.resolve(data); })
+        .error(function(err) { deferred.reject(err); });
+
+      return deferred.promise;
+    }
   }
   
 })();
