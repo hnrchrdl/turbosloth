337bcb859a1513ea9416b7f7817ef306b5044215
hnrchrdl <hinrich@posteo.de>
2015-01-20 13:46:40 +0100

    Create userCtrl.js

---
+++ controller/userCtrl.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+  login: login,
+  logout: logout,
+  authenticate: authenticate
+}
+
+
+//////////////////////////////////////////
+
+function login(req,res) {}
+
+function logout(req,res) {}
+
+function authenticate(req,res){}
