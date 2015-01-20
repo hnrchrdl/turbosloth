c9c209291672875bb1dc9551216f9f730ef04f48
hnrchrdl <hinrich@posteo.de>
2015-01-20 14:32:02 +0100

    Create mpd.js

---
+++ controller/mpd.js
@@ -0,0 +1,20 @@
+
+
+module.exports = {
+  getList: getList,
+  select: select,
+  test: test,
+  save: save
+}
+
+
+/////////////////////////////////////////////////////
+
+
+function getList(req,res) {}
+
+function select (req,res) {}
+
+function test(req,res) {}
+
+function save(req,res) {}
