725ed602fba07e1c574f03e82f96e1f246b939bf
hnrchrdl <hinrich@posteo.de>
2015-02-16 15:23:25 +0100

    Update browse.factory.js

--- public/src/app/common/factories/browse.factory.js
+++ public/src/app/common/factories/browse.factory.js
@@ -3,7 +3,31 @@
   angular.module('app')
     .factory('BrowseFactory', BrowseFactory);
     
-  function BrowseFactory() {
+  function BrowseFactory($http, $q) {
+    
+    return {
+      browseFolder: browseFolder
+    };
+    
+    /////////////////////////////////
+    
+    function browseFolder(folder) {
+      
+      var deferred = $q.defer();
+      
+      if (folder) {
+        var http = $http.get('/api/browse/' + folder);
+      }
+      else {
+        var http = $http.get('/api/browse');
+      }
+      
+      http
+        .success(function(data) { deferred.resolve(data); })
+        .error(function(err) { deferred.reject(err); });
+      
+      return deferred.promise;
+    }
     
   }
   
